d9aea37b457e5806dab2807f1bb2c89124c224cf
Ariflutfi <arif.kurniawan@students.amikom.ac.id>
2022-12-27 16:55:40 +0700

    update

--- app/src/main/java/com/pcs/apptoko/ProdukFragment.kt
+++ app/src/main/java/com/pcs/apptoko/ProdukFragment.kt
@@ -5,15 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.pcs.apptoko.api.BaseRetrofit
 
 
 class ProdukFragment : Fragment() {
+
+    private val api by lazy { BaseRetrofit().endpoint }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_produk, container, false)
+
+
+
         return view
     }
 }
